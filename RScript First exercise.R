a809adc6d223bd0468a08f1fef8a98da955d80c1
JustierNo1 <patrick.glettig@gmail.com>
2018-02-28 15:20:58 +0100

    Last commit was wrong. Copied Python script.

--- RScript First exercise.R
+++ RScript First exercise.R
@@ -6,73 +6,47 @@
 #get libraries
 library(igraph)
 
-#define functions
 
-#This function is used to create a basis population with some infected individuals
+pop <- 5000
+n_0 <- 5
+beta <- 5
+gamma <- 1
+recovered <- c()
+infected <- c()
+susceptible <- c()
 
-initialize <- function(Size, n_0 = 5){
-  population <- rep(0, Size)
-  infected <- sample(Size, size = n_0)
-  population[infected] <- 1
-  return(population)
+s <- rep(0, pop)
+
+for (i in 1:n_0){
+  s[sample(pop, size = 1)] <- 1
 }
 
-#Let's test if the function works
-example <- initialize(5000, 5)
-sum(example==1)
-
-#The counting functions
-
-get_number_infected <- function(x){
-  sum(x==1)
-}
-
-get_number_recovered <- function(x){
-  sum(x==2)
-}
-
-get_number_susceptibles <- function(x){
-  sum(x==0)
-}
-
-#The Sir evolve algorithm
-sir_evolve <- function(n, b , g){
-  size <- length(n)
-  pos_i <- which(n==1)
+iter <- 0
+while ((sum(s==2) < pop)){
+  pos_i <- which(s==1)
+  recovered[i] <- sum(s==2)#recovered
+  infected[i] <- sum(s==1)#infected
+  susceptible[i] <- sum(s==0)#susceptible
   for (i in 1:length(pos_i)){
-    if (runif(1) < (g/(b+g))){
+    if (runif(1) < (gamma/(beta+gamma))){
       #recovery
-      i <- sample(pos_i, 1)
-      n[i] <- 2
+      s[i] <- 2
     } else {
       #infection
-      i <- sample(pos_i, 1)
-      j <- sample(1:size,1)
-        if (n[i] == 0){
-          n[j] <- 1
-          }
+      j <- sample(pop-1,1)
+      if (s[j] == 0){
+        s[j] <- 1
       }
     }
-}
-
-#Testing the algorithm. Be careful about infinite loops.
-iter <- 0
-while (get_number_recovered(example) < length(example)){
-  sir_evolve(example,b = 20, g = 1)
-  print(get_number_infected(example))
-  iter <- iter +1
+  }
+  print(c(sum(s==0),sum(s==1),sum(s==2)))
+  iter <- iter+1
 }
 print(iter)
 
-N_0 <- 1 #initial number
-
-
-#Parameters
-
-b <- seq(from = 0.1, to = 2, by=0.01) #infection probability
-g <- 1 #recovery probability
-N <- 5*10^3 #system size
-
-
-R_0 <- b/g #basic reproduction rate
+my_int <- beta/gamma
+Recovered_divided <- c()
+for (i in 1:length(recovered)){
+  Recovered_divided[i] <- recovered[i]/my_int
+}
 
